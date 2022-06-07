c3dcf78f9f705aeeb6c29e42b9d0bf288d4b8b28
andriyansyah0003 <andriyansyah0003@gmail.com>
2022-06-07 10:58:02 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "10rem",
+    },
+    extend: {
+      colors: {
+        primary: "#00df9a",
+      },
+    },
+    plugins: [],
+  },
+};
